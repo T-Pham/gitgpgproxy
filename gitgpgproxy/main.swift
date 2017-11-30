49737a4a3bb33bd81f254a3c535d954a9a2265de
Thanh Pham <minhthanh@me.com>
2017-11-30 15:19:42 +0800

    Initial Commit

---
+++ gitgpgproxy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gitgpgproxy
+//
+//  Created by Thanh Pham on 30/11/17.
+//  Copyright © 2017 Thanh Pham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
