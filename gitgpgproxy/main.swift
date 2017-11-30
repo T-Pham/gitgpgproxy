1b06578d228a572ede4eeafc78d0f0dc213a5da6
Thanh Pham <minhthanh@me.com>
2017-11-30 16:50:13 +0800

    add the code

--- gitgpgproxy/main.swift
+++ gitgpgproxy/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+@discardableResult
+func exec(_ arguments: [String]) -> Int32 {
+  let task = Process()
+  task.launchPath = "/usr/bin/env"
+  task.arguments = arguments
+  task.launch()
+  task.waitUntilExit()
+  return task.terminationStatus
+}
 
+var arguments = CommandLine.arguments
+arguments[0] = "git"
+if arguments.count > 1 && (arguments[1] == "commit" || arguments[1] == "commit-tree") {
+  arguments.insert("-S", at: 2)
+}
+
+let result = exec(arguments)
+exit(result)
